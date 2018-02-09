2d0b761daa3ed0cbb8419faada07fa781bfe0ef4
Juan Carlos hurtado <juankahurtado.14@gmail.com>
2018-02-09 15:15:40 -0500

    Login continue...

---
+++ Lands/Lands/Infrastructure/InstanteLocator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lands.Infrastructure
+{
+    using ViewModels;
+
+    class InstanteLocator
+    {
+
+        #region Properties
+
+        public MainViewModel Main
+        {
+            get;
+            set;
+        }
+
+        #endregion
+
+        #region Constructor
+
+        public InstanteLocator()
+        {
+            this.Main = new MainViewModel();
+        }
+
+        #endregion
+    }
+}
---
+++ Lands/Lands/ViewModels/LoginViewModel.cs
@@ -0,0 +1,96 @@
+﻿
+namespace Lands.ViewModels
+{
+    using GalaSoft.MvvmLight.Command;
+    using System;
+    using System.Net.Mail;
+    using System.Windows.Input;
+    using Xamarin.Forms;
+
+    public class LoginViewModel
+    {
+        #region Properties
+        public string Email
+        {
+            get;
+            set;
+        }
+
+        public string Password
+        {
+            get;
+            set;
+        }
+
+        public bool IsRunning
+        {
+            get;
+            set;
+        }
+
+        public bool IsRemembered
+        {
+            get;
+            set;
+        }
+        #endregion
+
+        #region Constructores
+        public LoginViewModel()
+        {
+            this.IsRemembered = true;
+            this.Email = "Juan@a.com";
+        }
+        #endregion
+
+        #region Commands
+        public ICommand LoginCommand
+        {
+            get
+            {
+                return new RelayCommand(Login);
+            }
+        }
+
+        private async void Login()
+        {
+            if (string.IsNullOrEmpty(this.Email))
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error",
+                    "You must enter an email.",
+                    "Accept");
+                return;
+            }
+            else
+            {
+                try
+                {
+                    MailAddress m = new MailAddress(this.Email);
+
+                }
+                catch (FormatException)
+                {
+                    await Application.Current.MainPage.DisplayAlert(
+                     "Error",
+                     "It must be email type ",
+                     "Accept");
+                    return;
+                }
+            }
+
+
+            
+
+            if (string.IsNullOrEmpty(this.Password))
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error",
+                    "You must enter a password.",
+                    "Accept");
+                return;
+            }
+        }
+        #endregion
+    }
+}
